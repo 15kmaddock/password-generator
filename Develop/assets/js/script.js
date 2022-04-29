ca7346f504c316c352541247d796ca515240303f
kody maddock <kodymaddock15k@hotmail.com>
2022-04-29 01:32:12 -0600

    added arrays

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,5 +1,36 @@
 // Assignment code here
+// Array for characters
+const lchar = "qwertyuiopasdfghjklzxcvbnm";
+const uchar = "QWERTYUIOPASDFGHJKLZXCVBNM";
+const symbols = "!@#$%^&*_-+=";
+const numbers = "1234567890"
 
+const lowercase = lchar
+const uppercase = uchar
+const charSymbols = symbols
+const charNumbers = numbers
+
+function generatePassword () {
+  const length = prompt ('Enter your desired password length of 8 - 128 characters');
+  if (length <=7) {
+    window.alert ('password length not sufficient');
+    return length();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const password = document.getElementById
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
