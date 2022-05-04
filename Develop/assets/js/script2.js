1a636fc7827626874da0b882298f6d94029204f4
Kody Maddock <kodymaddock15k@hotmail.com>
2022-05-04 16:43:55 -0600

    generate provides true for password

--- Develop/assets/js/script2.js
+++ Develop/assets/js/script2.js
@@ -3,15 +3,17 @@ var uChar = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var symbols = "!@#$%^&*-_=+";
 var numbers = "1234567890";
 var userInput = [];
-var random = [];
+var randomPassword = [];
 
+// Get references to #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #passwrd input
 function writePassword() {
-    
-    var length = promt ("Select password length form 8-128 characters.");
-    if (length <=7 || length >=129) 
-    return length ();
+
+var length = prompt("Select password length form 8-128 characters.");
+if (length < 7 || length > 129) {
+    return
 };
 
 var lChar = confirm("would you like to include lowercase characters?");
@@ -19,7 +21,30 @@ if (lChar) {
     userInput.push(lChar)
 };
 
-var uChar = confirm("Would you like to include uppercae charracters?");
+var uChar = confirm("Would you like to include uppercae characters?");
 if (uChar) {
     userInput.push(uChar)
 };
+
+var symbols = confirm("Would you like to include uppercase characters?");
+if (symbols) {
+    userInput.push(symbols)
+};
+
+var numbers = confirm("Would you like to include numerical characters?");
+if (numbers) {
+    userInput.push(numbers)
+};
+
+for (var i=0; i<length; i++) {
+    randomPassword.push(userInput[Math.floor(Math.random() * userInput.length)])
+  }
+
+  var text = document.querySelector("#password");
+  text.value = randomPassword.join("");
+
+};
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
